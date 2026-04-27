1530e01ddacb14e6403c1516c19006c29190b003
Viverun <khanajamilahmed202@gmail.com>
2026-04-27 17:49:37 +0000

    Testing

---
+++ tests.py
@@ -0,0 +1,16 @@
+"""Default backend test suite entrypoint for `manage.py test`."""
+
+from unittest import TestLoader, TestSuite
+
+
+TEST_MODULES = [
+    'apps.users.tests',
+    'apps.projects.tests',
+]
+
+
+def load_tests(loader: TestLoader, standard_tests, pattern):
+    suite = TestSuite()
+    for module in TEST_MODULES:
+        suite.addTests(loader.loadTestsFromName(module))
+    return suite
